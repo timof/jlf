1d64a4a895be065cffaf5064b8b6025f8049b3e4
Timo Felbinger <gittf@qipc.org>
2012-05-31 13:52:50 +0200

    cookie-probe logic now in index.php, login.php - removed from forms.php

--- pi/forms.php
+++ pi/forms.php
@@ -2,7 +2,7 @@
 
 function form_login() {
   open_fieldset( 'class=small_form,style=padding:2em;', we('Login','Anmelden') );
-    if( $GLOBALS['valid_cookie_received'] ) {
+    // if( $GLOBALS['valid_cookie_received'] ) {
       flush_problems();
       hidden_input( 'l', 'login' );
       bigskip();
@@ -20,14 +20,14 @@ function form_login() {
             quad();
             submission_button( 'action=,text='.we('log in','Anmelden') );
       close_table();
-    } else {
-      open_div( 'warn bigskips qquads' , we(" You seem to have disabled cookies in your browser. Please switch on cookie support for this site!  "
-                                         , " Cookie-Unterst&uuml;tzung ihres Browsers scheint ausgeschaltet zu sein. Bitte erlauben Sie Cookies f&uum;r diese Webseite! " ) );
-
-      open_div( 'right' );
-        submission_button( 'login=nop,text='.we('back', 'zurück') );
-      close_div();
-    }
+    // } else {
+//       open_div( 'warn bigskips qquads' , we(" You seem to have disabled cookies in your browser. Please switch on cookie support for this site!  "
+//                                          , " Cookie-Unterst&uuml;tzung ihres Browsers scheint ausgeschaltet zu sein. Bitte erlauben Sie Cookies f&uum;r diese Webseite! " ) );
+// 
+//       open_div( 'right' );
+//         submission_button( 'login=nop,text='.we('back', 'zurück') );
+//       close_div();
+//     }
       bigskip();
   close_fieldset();
 }
