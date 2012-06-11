749b906127c02947ad0080a86378fa86d510dbd9
Timo Felbinger <gittf@qipc.org>
2012-06-11 10:55:58 +0200

    harden group/person links to handle non-existing entries

--- pi/html.php
+++ pi/html.php
@@ -2,26 +2,34 @@
 
 function html_alink_person( $people_id, $opts = array() ) {
   $opts = parameters_explode( $opts, 'class' );
-  if( ! ( $text = adefault( $opts, 'text', false ) ) ) {
-    $person = sql_person( $people_id );
-    $text = $person['cn'];
+  $person = sql_person( $people_id, NULL );
+  if( $person ) {
+    if( ! ( $text = adefault( $opts, 'text', false ) ) ) {
+      $text = $person['cn'];
+    }
+    return inlink( 'person_view', array(
+      'people_id' => $people_id
+    , 'class' => adefault( $opts, 'class', 'href' )
+    , 'text' => $text
+    , 'title' => $text
+    ) );
+  } else {
+    return we('(no person)','(keine Person)');
   }
-  return inlink( 'person_view', array(
-    'people_id' => $people_id
-  , 'class' => adefault( $opts, 'class', 'href' )
-  , 'text' => $text
-  , 'title' => $text
-  ) );
 }
 
 function html_alink_group( $groups_id, $class = 'href' ) {
-  $group = sql_one_group( $groups_id );
-  return inlink( 'group_view', array(
-    'groups_id' => $groups_id
-  , 'class' => $class
-  , 'text' => $group['acronym']
-  , 'title' => $group['cn_we']
-  ) );
+  $group = sql_one_group( $groups_id, NULL );
+  if( $group ) {
+    return inlink( 'group_view', array(
+      'groups_id' => $groups_id
+    , 'class' => $class
+    , 'text' => $group['acronym']
+    , 'title' => $group['cn_we']
+    ) );
+  } else {
+    return we('(no group)','(keine Gruppe)');
+  }
 }
 
 ?>
