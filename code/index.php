2b920dd11ff8be90ea87d0aacd1d17052a113de0
Timo Felbinger <gittf@qipc.org>
2012-06-26 13:20:51 +0200

    cleanup: handling of global_format and global_context, parse format from me

--- code/index.php
+++ code/index.php
@@ -1,46 +1,78 @@
 <?php
 
-// reasonable defaults for very early scripts (in particular: login.php):
-//
-$script = 'menu';
-$window = 'menu';
-$thread = '1';
-$debug = 1; // good choice in case of very early errors
-
 // activate this line to see very early errors:
 //
 // echo "<!DOCTYPE HTML PUBLIC \"-//W3C//DTD HTML 4.01 Transitional//EN\">\n\n";
+
+$debug = 1; // good choice in case of very early errors
+$language = 'E';
 
 require_once('code/common.php');
 
 // POST parameter l is treated special: we evaluate and sanitize it early and store into $login; this can be
 // used to pass small amounts of data e.g. to the code handling login, before a session is established:
 //
-if( isset( $_POST['l'] ) ) {
-  $login = (string)$_POST['l'];
-  unset( $_POST['l'] );
-  if( ! preg_match( '/^[A-Za-z_]{1,16}$/', $login ) )
-    $login = '';
-} else {
+$login = adefault( $_POST, 'l', '' );
+unset( $_POST['l'] );
+if( ! preg_match( '/^[A-Za-z_]{1,32}$/', $login ) ) {
   $login = '';
 }
 
-switch( $login ) {
-  case 'Lcsv':
-    $global_format = 'csv';
-    $login = '';
+$me = adefault( $_GET, 'me', 'menu,menu,1' );
+unset( $_GET['me'] );
+need( preg_match( '/^[a-zA-Z0-9_,]*$/', $me ) );
+
+$me = explode( ',', $me );
+$script = adefault( $me, 0, 'menu' );
+$window = adefault( $me, 1, 'menu' );
+$thread = adefault( $me, 2, '1' );
+need( preg_match( '/^[1-4]$/', $thread ) );
+$parent_script = adefault( $me, 3, $script );
+$parent_script or $parent_script = $script;
+$parent_window = adefault( $me, 4, $window );
+$parent_window or $parent_window = $window;
+$parent_thread = adefault( $me, 5, $thread );
+$parent_thread or $parent_thread = $thread;
+need( preg_match( '/^[1-4]$/', $parent_thread ) );
+$global_format = adefault( $me, 6, 'html' );
+
+switch( $window ) {
+  case 'DIV':
+    $global_context = CONTEXT_DIV;
     break;
-  case 'Lpdf':
-    $global_format = 'pdf';
-    $login = '';
+  case 'IFRAME':
+    $global_context = CONTEXT_IFRAME;
     break;
   default:
-    $global_format = 'html';
+    $global_context = CONTEXT_WINDOW;
+    break;
+}
+switch( $global_format ) {
+  case 'csv':
+    // header( 'Content-Type: text/force-download' );
+    header( 'Content-Type: text/plain' );
+    // header( 'Content-Disposition: attachement; filename="'.$script.'.csv"' );
+    $global_context = CONTEXT_DOWNLOAD;
+    break;
+  case 'pdf':
+    header( 'Content-Type: application/pdf' );
+    header( 'Content-Disposition: attachement; filename="'.$script.'.pdf"' );
+    $global_context = CONTEXT_DOWNLOAD;
+    break;
+  case 'html':
+    break;
+  default:
+    error( 'unsupported global_format', LOG_FLAG_CODE, 'init' );
 }
 
+// the following must come early before any output is printed:
+//
 $cookie_support = check_cookie_support();
-
 handle_login();
+
+// start output now - the htmlDefuse filter will gobble everything up to the "format:"-line:
+//
+echo "\n\n  ERROR: if you see this line in browser, you need to configure htmlDefuse as ExtFilter for your apache server! \n\n";
 
 if( function_exists( 'init_session' ) ) {
   init_session( $login_sessions_id );
@@ -48,57 +80,33 @@ if( function_exists( 'init_session' ) ) {
 
 if( $login_sessions_id ) {
 
-  init_var( 'me', array( 'global' => true, 'sources' => 'http', 'default' => 'menu,menu,1' ) );
-
-  $me = explode( ',', $me );
-  $script = adefault( $me, 0, 'menu' );
-  $window = adefault( $me, 1, 'menu' );
-  $thread = adefault( $me, 2, '1' );
-  need( preg_match( '/^[1-4]$/', $thread ) );
-  $parent_script = adefault( $me, 3, $script );
-  $parent_script or $parent_script = $script;
-  $parent_window = adefault( $me, 4, $window );
-  $parent_window or $parent_window = $window;
-  $parent_thread = adefault( $me, 5, $thread );
-  $parent_thread or $parent_thread = $thread;
-  need( preg_match( '/^[1-4]$/', $parent_thread ) );
-
-  $script_basename = basename( $_SERVER['SCRIPT_URL'] ); // SCRIPT_URL: script relative to document root
-  if( $script_basename == 'index.php' ) { // SCRIPT_URL: script relative to document root
-    switch( $global_format ) {
-      case 'html':
-        switch( $window ) {
-          case 'IFRAME': // complete html, but no browser window
-            $global_context = CONTEXT_IFRAME;
-            break;
-          case 'DIV':  // html fragment only
-            $global_context = CONTEXT_DIV;
-            break;
-          default:  // complete html with :
-            $global_context = CONTEXT_WINDOW;
-            break;
-        }
-        break;
-      case 'pdf':
-        header( 'Content-Type: application/pdf' );
-        $global_context = CONTEXT_DIV; // not really, but we don't need the html header and stuff either in the .pdf
-        break;
-      case 'csv':
-        header( 'Content-Type: text/plain' );
-        $global_context = CONTEXT_DIV;
-        break;
+  // $script_basename = basename( $_SERVER['SCRIPT_URL'] ); // SCRIPT_URL: script relative to document root
+  // if( $script_basename === 'index.php' ) { // SCRIPT_URL: script relative to document root
+  if( $global_format === 'html' ) {
+    if( $global_context >= CONTEXT_WINDOW ) {
+      js_on_exit( sprintf( "window.name = {$H_SQ}%s{$H_SQ};", js_window_name( $window, $thread ) ) );
     }
-  } else if( $script_basename == 'get.rphp' ) {
-    $global_context = CONTEXT_DOWNLOAD;
-    $script = 'download';
-  } else if( $script_basename == 'get.php' ) {
-    $global_context = CONTEXT_DIV;
-  } else {
-    error( 'invalid script requested: ['. $script_basename . ']', LOG_FLAG_INPUT | LOG_FLAG_CODE, 'links' );
+    // all GET requests via load_url() and POST requests via submit_form() will pass current window scroll
+    // position in paramater xoffs. restore position for 'self'-requests:
+    //
+    if( ( $parent_script === 'self' ) && ( $global_context >= CONTEXT_IFRAME ) ) {
+      // restore scroll position:
+      $offs_field = init_var( 'offs', 'sources=http,default=0x0' );
+      if( preg_match( '/^(\d+)x(\d+)$/', $offs_field['value'], & $matches ) ) {
+        $xoff = $matches[ 1 ];
+        $yoff = $matches[ 2 ];
+        js_on_exit( "window.scrollTo( $xoff, $yoff ); " );
+      }
+    }
   }
-
-  if( $global_context >= CONTEXT_WINDOW )
-    js_on_exit( sprintf( "window.name = {$H_SQ}%s{$H_SQ};", js_window_name( $window, $thread ) ) );
+  //   } else if( $script_basename == 'get.rphp' ) {
+  //     $global_context = CONTEXT_DOWNLOAD;
+  //     $script = 'download';
+  //   } else if( $script_basename == 'get.php' ) {
+  //     $global_context = CONTEXT_DIV;
+  // } else {
+  //   error( 'invalid script requested: ['. $script_basename . ']', LOG_FLAG_INPUT | LOG_FLAG_CODE, 'links' );
+  // }
 
   retrieve_all_persistent_vars();
 
@@ -110,7 +118,7 @@ if( $login_sessions_id ) {
     include( "$jlf_application_name/common.php" );
   }
 
-  // head.php:
+  // head.php: if global_format is 'html',
   // - print <doctype> babble and html <head> section
   // - open update form
   // - output visible page head
@@ -123,8 +131,7 @@ if( $login_sessions_id ) {
     fork_new_thread();
   }
 
-
-  if( $login == 'login' ) { // request: show paleolithic-style login form:
+  if( $login === 'login' ) { // request: show paleolithic-style login form:
     form_login();
   } else {
     switch( $login_authentication_method ) {
@@ -142,51 +149,35 @@ if( $login_sessions_id ) {
     }
   }
 
-  // all GET requests via load_url() and POST requests via submit_form() will pass current window scroll
-  // position in paramater xoffs. restore position for 'self'-requests:
-  //
-  if( $parent_script === 'self' ) {
-    // restore scroll position:
-    $offs_field = init_var( 'offs', 'sources=http,default=0x0' );
-    if( preg_match( '/^(\d+)x(\d+)$/', $offs_field['value'], & $matches ) ) {
-      $xoff = $matches[ 1 ];
-      $yoff = $matches[ 2 ];
-    } else {
-      $xoff = $yoff = 0;
-    }
-    if( $global_context >= CONTEXT_IFRAME ) {
-      js_on_exit( "window.scrollTo( $xoff, $yoff ); " );
-    }
-  }
-
   set_persistent_var( 'thread_atime', 'thread', $utc );
   store_all_persistent_vars();
 
   include( "$jlf_application_name/footer.php" );
 
-} else {
+} else if( $global_format === 'html' ) {
   switch( $cookie_support ) {
     case 'fail':
-      html_header_view( 'please activate cookie support in your browser' );
+      header_view( 'html', 'please activate cookie support in your browser' );
       open_div( 'bigskips warn', 'please activate cookie support in your browser / Bitte cookie-Unterstützung ihres Browsers einschalten!' );
       break;
     case 'probe':
-      setcookie( cookie_name(), 'probe', 0, '/' );
-      html_header_view( 'checking cookie support...' );
+      header_view( 'html', 'checking cookie support...' );
       js_on_exit( "/* alert( {$H_SQ}sending cookie probe{$H_SQ} ); */ submit_form( {$H_SQ}update_form{$H_SQ}, $H_SQ$H_SQ, {$H_SQ}cookie_probe{$H_SQ} );" );
       exit();
    case 'ignore':
      // todo: how to handle robots - creating a new session on every access is not quite good.
      break;
    case 'ok':
-      html_header_view( 'access denied / kein Zugriff' );
+      header_view( 'html', 'access denied / kein Zugriff' );
       open_div( 'bigskips warn', 'access denied: no public access / kein öffentlicher Zugriff' );
       break;
    default:
       error( 'unexpected value for $cookie_support', LOG_FLAG_CODE, 'sessions,cookie' );
   }
+} else {
+  // not in html mode - cannot do much here:
+  echo "request failed: no session\n";
 }
-
 
 if( substr( get_itan(), -2 ) == '00' ) {
   logger( 'starting garbage collection', LOG_LEVEL_NOTICE, LOG_FLAG_SYSYEM, 'maintenance' );
