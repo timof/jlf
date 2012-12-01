d943cae093bf08c87dc09c06de0fde0d78fff76d
Timo Felbinger <gittf@qipc.org>
2012-12-01 14:57:13 +0100

    year 2012

--- sus/windows/ka.php
+++ sus/windows/ka.php
@@ -37,6 +37,17 @@ foreach( $darlehen as $d ) {
     $posten_zins_auszahlung = sql_posten( "unterkonten_id=$zins_uk_id,art=S,valuta>100" );
   }
 
+  $vortrag_saldo = 0.0;
+  foreach( $posten_darlehen_vortrag as $p ) {
+    $vortrag_saldo += $p['betrag'];
+  }
+  foreach( $posten_zins_vortrag as $p ) {
+    $vortrag_saldo += $p['betrag'];
+  }
+  if( $vortrag_saldo < 0.01 ) {
+    continue;
+  }
+
   $du = ( $person['dusie'] == 'D' );
   
   echo "\n=========================================================================================\n";
@@ -73,8 +84,13 @@ foreach( $darlehen as $d ) {
       $kondition = 'K';
       break;
     case 2021:
-      echo "L (Laufzeit 20 Jahre, 10 Jahre tilgungsfrei)";
-      $kondition = 'L';
+      if( $d['geschaeftsjahr_zinsauszahlung_start'] <= $geschaeftsjahr_current ) {
+        echo "LA (Laufzeit 20 Jahre, 10 Jahre tilgungsfrei, jaehrliche Zinsausschuettung)";
+        $kondition = 'LA';
+      } else {
+        echo "L (Laufzeit 20 Jahre, 10 Jahre tilgungsfrei, Zinsansammlung bis 2021)";
+        $kondition = 'L';
+      }
       break;
     default:
       echo "*** SONDERFALL - manuell pruefen ***";
@@ -84,6 +100,33 @@ foreach( $darlehen as $d ) {
   echo "\n";
   echo "kommentar: {$person['note']}\n";
 
+  $saldo_darlehen = 0;
+  $saldo_zins = 0;
+  $posten = '';
+  foreach( $posten_darlehen_vortrag as $p ) {
+    $posten .= sprintf( "  Vortrag Darlehen aus 2011:     %8.2lf\n", $p['betrag'] );
+    $saldo_darlehen += $p['betrag'];
+  }
+  foreach( $posten_zins_vortrag as $p ) {
+    $posten .= sprintf( "  Vortrag Zins aus 2011:         %8.2lf\n", $p['betrag'] );
+    $saldo_zins += $p['betrag'];
+  }
+  foreach( $posten_zins_gutschrift as $p ) {
+    $posten .= sprintf( "  Zinsgutschrift fuer 2012:      %8.2lf\n", $p['betrag'] );
+    $saldo_zins += $p['betrag'];
+  }
+  foreach( $posten_darlehen_tilgung as $p ) {
+    $posten .= sprintf( "  Tilgungszahlung Ende 2012:     %8.2lf\n", -$p['betrag'] );
+    $saldo_darlehen -= $p['betrag'];
+  }
+  foreach( $posten_zins_auszahlung as $p ) {
+    $posten .= sprintf( "  Zinsauszahlung Ende 2012:      %8.2lf\n", -$p['betrag'] );
+    $saldo_zins -= $p['betrag'];
+  }
+  if( $saldo_zins + $saldo_darlehen < 0.01 ) {
+    echo "*** Darlehen ist getilgt ***\n\n";
+    continue;
+  }
   echo "------------------------------------------------------------------------------\n";
   echo "From: UniSolar Potsdam e.V. {$H_LT}info@unisolar-potsdam.de{$H_GT}\n";
   echo "To: {$person['cn']} $H_LT{$person['mail']}$H_GT\n";
@@ -96,37 +139,15 @@ foreach( $darlehen as $d ) {
   echo "\n";
   echo "bereits seit zwei Jahren ist nun unsere Photovoltaik-Anlage auf Haus 6 am\n";
   echo "Golm der Universitaet Potsdam in Betrieb; mit einem Darlehen in Hoehe von\n";
-  echo "{$d['betrag_abgerufen']} Euro " . ( $du ? 'hast Du' : 'haben Sie' ) . "mitgeholfen,\n";
-  echo "diese Anlage zu finanzieren.";
+  echo "{$d['betrag_abgerufen']} Euro " . ( $du ? 'hast Du' : 'haben Sie' ) . " mitgeholfen, diese Anlage zu finanzieren.\n";
   echo "\n";
-  echo "Auch in diesem Jahr liegt der Ertrag der Anlage mit bisher mehr als 31.000 kWh\n";
+  echo "Der Ertrag der Anlage liegt mit bisher mehr als 31.000 kWh auch in diesem Jahr\n";
   echo "bereits deutlich ueber der Prognose. Ein Ueberschuss von mehr als 2000 Euro aus\n";
-  echo "dem Ertrag der Anlage wird in diesem Jahr fuer gemeinnuetzige Arbeit, wie etwa\n";
+  echo "der Einspeiseverguetung wird in diesem Jahr fuer gemeinnuetzige Arbeit, wie etwa\n";
   echo "den Kongress 'Energiedemokratie' am vorletzten Wochenende, zur Verfuegung stehen.\n";
   echo "\n";
   echo "Der Stand " . ( $du ? 'Deines' : 'Ihres' ) ." Darlehens zum Ende des laufenden Jahres 2012:\n";  
-  $saldo_darlehen = 0;
-  $saldo_zins = 0;
-  foreach( $posten_darlehen_vortrag as $p ) {
-    printf( "  Vortrag Darlehen aus 2011:     %8.2lf\n", $p['betrag'] );
-    $saldo_darlehen += $p['betrag'];
-  }
-  foreach( $posten_zins_vortrag as $p ) {
-    printf( "  Vortrag Zins aus 2011:         %8.2lf\n", $p['betrag'] );
-    $saldo_zins += $p['betrag'];
-  }
-  foreach( $posten_zins_gutschrift as $p ) {
-    printf( "  Zinsgutschrift fuer 2012:      %8.2lf\n", $p['betrag'] );
-    $saldo_zins += $p['betrag'];
-  }
-  foreach( $posten_darlehen_tilgung as $p ) {
-    printf( "  Tilgungszahlung Ende 2012:     %8.2lf\n", -$p['betrag'] );
-    $saldo_darlehen -= $p['betrag'];
-  }
-  foreach( $posten_zins_auszahlung as $p ) {
-    printf( "  Zinsauszahlung Ende 2012:      %8.2lf\n", -$p['betrag'] );
-    $saldo_zins -= $p['betrag'];
-  }
+  echo $posten;
   printf( "  ----------------------------------------\n", $saldo_darlehen );
   printf( "  Saldo Darlehen am 31.12.2012:  %8.2lf\n", $saldo_darlehen );
   printf( "  Saldo Zinskonto am 31.12.2012: %8.2lf\n", $saldo_zins );
@@ -147,44 +168,50 @@ foreach( $darlehen as $d ) {
   echo "  Konto-Nr: {$person['bank_kontonr']}\n";
   echo "Falls diese Angaben unrichtig oder unvollstaendig sein sollten, " 
        . ( $du ? 'lass uns ' : 'lassen Sie uns ' ) . "das bitte\n";
-  echo "wissen!\n";
+  echo "umgehend wissen!\n";
   echo "\n";
-  echo "Auch in diesem Jahr sind wir in der Lage und daran interessiert, in begrenztem\n";
-  echo "Umfang Sondertilgungen zu leisten.\n";
-  echo "Wenn " . ( $du ? 'Du' : 'Sie' ) . " Interesse an einer vorzeitigen Rueckzahlung "
+  switch( $kondition ) {
+    case 'L':
+      echo "Nach unserer Vereinbarung wird die Tilgung des Darlehens und die Auszahlung\n";
+      echo "der Zinsen Ende des Jahres 2021 beginnen.\n";
+      echo "\n";
+      echo "Aufgrund des guten Ertrages sind wir aber auch in diesem Jahr in der Lage und\n";
+      echo "daran interessiert, in begrenztem Umfang Sondertilgungen zu leisten.\n";
+      echo "Wenn " . ( $du ? 'Du' : 'Sie' ) . " Interesse an einer vorzeitigen Rueckzahlung "
            . ( $du ? 'hast' : 'haben' ) . ", dann " . ( $du ? 'schreibe' : 'schreiben Sie' ) . " uns bitte!\n";
+      break;
+    case 'LA':
+      echo "Nach unserer Vereinbarung werden Zinsen jaehrlich ausgezahlt; die Tilgung des\n";
+      echo "Darlehens beginnt Ende des Jahres 2021.\n";
+      echo "\n";
+      echo "Aufgrund des guten Ertrages sind wir aber auch in diesem Jahr in der Lage und\n";
+      echo "daran interessiert, in begrenztem Umfang Sondertilgungen zu leisten.\n";
+      echo "Wenn " . ( $du ? 'Du' : 'Sie' ) . " Interesse an einer vorzeitigen Rueckzahlung "
+           . ( $du ? 'hast' : 'haben' ) . ", dann " . ( $du ? 'schreibe' : 'schreiben Sie' ) . " uns bitte!\n";
+      break;
+    case 'K':
+      echo "Nach unserer Vereinbarung wird die Rueckzahlung des Darlehens und die Auszahlung\n";
+      echo "der Zinsen Ende des Jahres 2020 erfolgen.\n";
+      echo "\n";
+      echo "Aufgrund des guten Ertrages sind wir aber auch in diesem Jahr in der Lage und\n";
+      echo "daran interessiert, in begrenztem Umfang Sondertilgungen zu leisten.\n";
+      echo "Wenn " . ( $du ? 'Du' : 'Sie' ) . " Interesse an einer vorzeitigen Rueckzahlung "
+           . ( $du ? 'hast' : 'haben' ) . ", dann " . ( $du ? 'schreibe' : 'schreiben Sie' ) . " uns bitte!\n";
+      break;
+    case 'R':
+      echo "Aufgrund des guten Ertrages sind wir auch in diesem Jahr in der Lage und\n";
+      echo "daran interessiert, in begrenztem Umfang Sondertilgungen zu leisten.\n";
+      echo "Wenn " . ( $du ? 'Du' : 'Sie' ) . " Interesse an einer rascheren Rueckzahlung "
+           . ( $du ? 'hast' : 'haben' ) . ", dann " . ( $du ? 'schreibe' : 'schreiben Sie' ) . " uns bitte!\n";
+#       echo "Aufgrund des guten Ertrags der PV-Anlage sind wir in der Lage und interessiert daran,\n";
+#       echo "in begrenztem Umfang Sondertilgungen zu leisten.\n";
+#       echo "Wenn " . ( $du ? 'Du' : 'Sie' ) . " Interesse an einer vorzeitigen Rueckzahlung "
+#            . ( $du ? 'hast' : 'haben' ) . ", dann " . ( $du ? 'schreibe' : 'schreiben Sie' ) . " uns bitte!\n";
+      break;
+  }
 
-//   switch( $kondition ) {
-//     case 'L':
-//       echo "Nach unserer Vereinbarung wird die Tilgung des Darlehens und die Auszahlung\n";
-//       echo "der Zinsen erst in 10 Jahren beginnen. Wir wuerden aber gern einen Teil unserer\n";
-//       echo "Schulden schon rascher als urspruenglich geplant abbauen und bieten "
-//             . ( $du ? 'Dir' : 'Ihnen' ) . " daher\n";
-//       echo "an, den Vertrag auf jaehrliche Zinsausschuettung umzustellen - in den ersten\n";
-//       printf( "10 Jahren waeren das %.2lf Euro pro Jahr; da Zinsen einkommensteuerpflichtig\n", $d['betrag_abgerufen'] * $d['zins_prozent'] / 100.0 );
-//       echo "sind, kann das unter Umstaenden vorteilhaft fuer " . ( $du ? 'Dich' : 'Sie' ). " sein.\n";
-//       echo "Wenn " . ( $du ? 'Du' : 'Sie' ) . " dieses Angebot annehmen "
-//            . ( $du ? 'moechtest' : 'moechten' ) . ", dann "
-//            . ( $du ? 'teile' : 'teilen Sie' ) . " uns das bitte moeglichst\n";
-//       echo "bald mit!\n";
-//       break;
-//     case 'K':
-//       echo "Nach unserer Vereinbarung wird die Rueckzahlung des Darlehens und die Auszahlung\n";
-//       echo "der Zinsen in 9 Jahren erfolgen.\n";
-//       echo "Aufgrund des guten Ertrags der PV-Anlage sind wir aber in der Lage und\n";
-//       echo "interessiert daran, in begrenztem Umfang Sondertilgungen zu leisten.\n";
-//       echo "Wenn " . ( $du ? 'Du' : 'Sie' ) . " Interesse an einer vorzeitigen Rueckzahlung "
-//            . ( $du ? 'hast' : 'haben' ) . ", dann " . ( $du ? 'schreibe' : 'schreiben Sie' ) . " uns bitte!\n";
-//       break;
-//     case 'R':
-//       echo "Aufgrund des guten Ertrags der PV-Anlage sind wir in der Lage und interessiert daran,\n";
-//       echo "in begrenztem Umfang Sondertilgungen zu leisten.\n";
-//       echo "Wenn " . ( $du ? 'Du' : 'Sie' ) . " Interesse an einer vorzeitigen Rueckzahlung "
-//            . ( $du ? 'hast' : 'haben' ) . ", dann " . ( $du ? 'schreibe' : 'schreiben Sie' ) . " uns bitte!\n";
-//       break;
-//   }
   echo "\n";
-  echo "Sonnige Gruesse,\n";
+  echo "Winterliche Gruesse,\n";
   echo "Timo Felbinger (fuer UniSolar Potsdam e.V.)\n";
   echo "\n";
 
