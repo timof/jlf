50a1e13e57ae7ed8d25d8ec8d7a961672e9c0bff
Timo Felbinger <gittf@qipc.org>
2012-12-01 15:18:24 +0100

    fix permissions

--- deploy.sh
+++ deploy.sh
@@ -300,7 +300,7 @@ chmod 755 ./sus/windows
 chmod 644 ./sus/windows/posten.php
 chmod 644 ./sus/windows/things.php
 chmod 644 ./sus/windows/hauptkontenliste.php
-chmod 600 ./sus/windows/ka.php
+chmod 644 ./sus/windows/ka.php
 chmod 744 ./sus/windows/journal.php
 chmod 644 ./sus/windows/1ka.php
 chmod 644 ./sus/windows/darlehenliste.php
