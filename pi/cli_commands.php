49960f23e479cd44d617df114476d8147e4d30e3
Timo Felbinger <gittf@qipc.org>
2013-03-10 15:15:11 +0100

    display-logic for multiple affiliations

--- pi/cli_commands.php
+++ pi/cli_commands.php
@@ -28,40 +28,84 @@ function cli_persondetails_html( $people_id ) {
     $s = "\n" . html_tag( 'div', 'warn', 'query failed - no such person' );
   } else {
     $s = trim( "{$person['title']} {$person['gn']} {$person['sn']}" ) . "\n";
-    $affiliations = sql_affiliations( "people_id=$people_id" );
     if( $person['jpegphoto'] ) {
       $s .= html_tag( 'span', 'style=float:right;'
       , html_tag( 'img', array( 'style' => 'max-width:180px;max-height:180px;' , 'src' => ( 'data:image/jpeg;base64,' . $person['jpegphoto'] ) ), NULL )
       );
     }
+    $emails = $phones = $faxes = $rooms = array();
+    $affiliations = sql_affiliations( "people_id=$people_id,flags&=".GROUPS_FLAG_LIST );
+    foreach( $affiliations as $aff ) {
+      if( ( $r = $aff['roomnumber' ] ) ) {
+        if( ! in_array( $r, $rooms ) ) {
+          $rooms[] = $r;
+        }
+      }
+      if( ( $r = $aff['mail' ] ) ) {
+        if( ! in_array( $r, $emails ) ) {
+          $emails[] = $r;
+        }
+      }
+      if( ( $r = $aff['telephonenumber' ] ) ) {
+        if( ! in_array( $r, $phones ) ) {
+          $phones[] = $r;
+        }
+      }
+      if( ( $r = $aff['facsimiletelephonenumber' ] ) ) {
+        if( ! in_array( $r, $faxes ) ) {
+          $faxes[] = $r;
+        }
+      }
+    }
     $s .= html_tag( 'table', 'class=noborder' ) ."\n";
       $s .= html_tag( 'tr' );
         $s .= html_tag( 'td', '', 'Name:' ) . html_tag( 'td', '', $person['gn'].' '.$person['sn'] );
       $s .= html_tag( 'tr', false ) ."\n";
+      if( count( $rooms ) === 1 ) {
+        $s .= html_tag( 'tr' );
+          $s .= html_tag( 'td', '', '_m4_de(Raum)_m4_en(Room):' ) . html_tag( 'td', '', $rooms[ 0 ] );
+        $s .= html_tag( 'tr', false ) ."\n";
+      }
+      if( count( $phones ) === 1 ) {
+        $s .= html_tag( 'tr' );
+          $s .= html_tag( 'td', '', '_m4_de(Telefon)_m4_en(Phone):' ) . html_tag( 'td', '', $phones[ 0 ] );
+        $s .= html_tag( 'tr', false ) ."\n";
+      }
+      if( count( $faxes ) === 1 ) {
+        $s .= html_tag( 'tr' );
+          $s .= html_tag( 'td', '', 'Fax:' ) . html_tag( 'td', '', $faxes[ 0 ] );
+        $s .= html_tag( 'tr', false ) ."\n";
+      }
+      if( count( $emails ) === 1 ) {
+        $s .= html_tag( 'tr' );
+          $s .= html_tag( 'td', '', 'Email:' ) . html_tag( 'td', '', html_obfuscate_email( $emails[ 0 ] ) );
+        $s .= html_tag( 'tr', false ) ."\n";
+      }
+
       foreach( $affiliations as $aff ) {
-        $s .= html_tag( 'tr', 'class=medskip' );
+        $s .= html_tag( 'tr', 'class=medskipb' );
           $t = $aff['groups_cn'];
           if( $aff['groups_url'] ) {
             $t = html_tag( 'a',  array( 'href' => $aff['groups_url'] ), $t );
           }
           $s .= html_tag( 'td', '', '_m4_de(Bereich)_m4_en(Group):' ) . html_tag( 'td', '', $t );
         $s .= html_tag( 'tr', false ) ."\n";
-        if( $aff['roomnumber'] ) {
+        if( $aff['roomnumber'] && ( count( $rooms ) > 1 ) ) {
           $s .= html_tag( 'tr' );
             $s .= html_tag( 'td', '', '_m4_de(Raum)_m4_en(Room):' ) . html_tag( 'td', '', $aff['roomnumber'] );
           $s .= html_tag( 'tr', false ) ."\n";
         }
-        if( $aff['telephonenumber'] ) {
+        if( $aff['telephonenumber'] && ( count( $phones ) > 1 ) ) {
           $s .= html_tag( 'tr' );
             $s .= html_tag( 'td', '', '_m4_de(Telefon)_m4_en(Phone):' ) . html_tag( 'td', '', $aff['telephonenumber'] );
           $s .= html_tag( 'tr', false ) ."\n";
         }
-        if( $aff['facsimiletelephonenumber'] ) {
+        if( $aff['facsimiletelephonenumber'] && ( count( $faxes ) > 1 ) ) {
           $s .= html_tag( 'tr' );
             $s .= html_tag( 'td', '', 'Fax:' ) . html_tag( 'td', '', $aff['facsimiletelephonenumber'] );
           $s .= html_tag( 'tr', false ) ."\n";
         }
-        if( $aff['mail'] ) {
+        if( $aff['mail'] && ( count( $emails ) > 1 ) ) {
           $s .= html_tag( 'tr' );
             $s .= html_tag( 'td', '', 'Email:' ) . html_tag( 'td', '', html_obfuscate_email( $aff['mail'] ) );
           $s .= html_tag( 'tr', false ) ."\n";
