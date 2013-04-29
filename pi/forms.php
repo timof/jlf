4dbeef172cf34c850a28f7bddc99f441cebed9f6
Timo Felbinger <gittf@qipc.org>
2013-04-29 18:22:10 +0200

    replace <table> by css-table

--- pi/forms.php
+++ pi/forms.php
@@ -1,26 +1,22 @@
 <?php
 
 function form_login() {
-  open_fieldset( '', we('Login','Anmelden') );
-    flush_problems();
-    bigskip();
-    open_table('fieldset,css=1');
-      open_tr('');
-        open_label( 'uid', 'td', we('user-id: ','Benutzerkennung: ') );
-        // cannot use string_element() here: would pass uid as P0_uid!
-        open_td( 'kbd', html_tag( 'input', 'class=td,type=text,size=12,name=uid,value=', NULL ) );
-      open_tr('medskip');
-        open_label( 'password', 'td', we('password: ','Passwort: ') );
-        open_td( 'kbd', html_tag( 'input', 'class=td,type=password,size=12,name=password,value=', NULL ) );
-      open_tr('medskip');
-        open_td();
-        open_td('right');
-          echo inlink( '', 'login=nop,class=button,text='.we('back', 'zurück') );
-          quad();
-          echo inlink( '', 'login=login,class=button,text='.we('log in','Anmelden') );
-    close_table();
-    hidden_input( 'l', 'login' ); // make 'login' the default action (when pressing ENTER in form)
+  open_fieldset( 'table', we('Login','Anmelden') );
+    open_tr('');
+      open_td( '', we('user-id: ','Benutzerkennung: ') );
+      // cannot use string_element() here: would pass uid as P0_uid!
+      open_td( 'kbd', html_tag( 'input', 'class=td,type=text,size=12,name=uid,value=', NULL ) );
+    open_tr('medskip');
+      open_td( '', we('password: ','Passwort: ') );
+      open_td( 'kbd', html_tag( 'input', 'class=td,type=password,size=12,name=password,value=', NULL ) );
+    open_tr('medskip');
+      open_td();
+      open_td('right');
+        echo inlink( '', 'login=nop,class=button,text='.we('back', 'zurück') );
+        quad();
+        echo inlink( '', 'login=login,class=button,text='.we('log in','Anmelden') );
   close_fieldset();
+    hidden_input( 'l', 'login' ); // make 'login' the default action (when pressing ENTER in form)
 }
 
 ?>
