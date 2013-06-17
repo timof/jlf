b411979ed31be184491be1cb66727258df062f5a
Timo Felbinger <gittf@qipc.org>
2013-06-17 07:38:16 +0200

    publications database - WIP

--- pi/windows/publication_edit.php
+++ pi/windows/publication_edit.php
@@ -1,7 +1,7 @@
 <?php
 
 init_var( 'flag_problems', 'global,type=b,sources=self,set_scopes=self' );
-init_var( 'positions_id', 'global,type=u,sources=self http,set_scopes=self' );
+init_var( 'publications_id', 'global,type=u,sources=self http,set_scopes=self' );
 
 $reinit = ( $action === 'reset' ? 'reset' : 'init' );
 
