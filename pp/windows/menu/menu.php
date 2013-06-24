88cc96d8804e17d7eb1c7ba9077e374e77318d1f
Timo Felbinger <gittf@qipc.org>
2013-06-24 16:22:49 +0200

    new public pages, some content improved

--- pp/windows/menu/menu.php
+++ pp/windows/menu/menu.php
@@ -1,6 +1,6 @@
 <?php
 
-echo html_tag( 'h2','', we('Studying Physics at the Institute','Physikstudium am Institut') );
+echo html_tag( 'h2','', we('Studying at the Institute','Studium am Institut') );
 
 echo html_tag( 'h2','bigskipt', we('Research at the Institute','Forschung am Institut') );
 
