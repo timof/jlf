0fd7b3dce3fcef28ad8fab0d6da773f44193e19d
Timo Felbinger <gittf@qipc.org>
2013-07-22 10:48:03 +0200

    anylist: beta

--- code/anylist.php
+++ code/anylist.php
@@ -23,11 +23,42 @@ need_priv( $table, 'list' );
 
 $cols = $tables[ $table ]['cols'];
   
+$tcols = array();
+foreach( $cols as $col => $props ) {
+  $tcols[ $col ] = "s,t,h=$col";
+}
 
-$rows = sql_query( $table );
 
-echo count( $rows );
+$list_options = handle_list_options( true, $table, $tcols );
 
+$rows = sql_query( $table, array( 'orderby' => $list_options['orderby_sql'] ) );
+$count = count( $rows );
+$limits = handle_list_limits( $list_options, $count );
+$list_options['limits'] = & $limits;
+
+$pri = $table.'_id';
+
+open_list( $list_options );
+  open_list_row('header');
+    foreach( $tcols as $c => $props ) {
+      open_list_cell( $c );
+    }
+  foreach( $rows as $r ) {
+    open_list_row();
+      foreach( $tcols as $c => $props ) {
+        $payload = $r[ $c ];
+        if( ! check_utf8( $payload ) ) {
+          $payload = html_span( 'bold italic', '(binary data)' );
+        } else if( preg_match( '/^([a-zA-Z0-9_]*_)?([a-zA-Z0-9]+)_id$/', $c, /* & */ $v ) ) {
+          $payload = entry_link( $v[ 2 ], $payload );
+        } else {
+          $payload = substr( $payload, 0, 32 );
+        }
+        open_list_cell( $c, $payload );
+      }
+  }
+
+close_list();
 
 
 ?>
