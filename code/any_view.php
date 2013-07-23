8b4cab9d8e859c63ecc90bbdfccb5368449d5c2c
Timo Felbinger <gittf@qipc.org>
2013-07-23 14:47:18 +0200

    support display of references to entry render . in green

--- code/any_view.php
+++ code/any_view.php
@@ -2,6 +2,9 @@
 
 init_var( 'any_id', 'global,type=U,sources=http persistent,set_scopes=self' );
 init_var( 'table', 'global,type=W,sources=http persistent,set_scopes=self' );
+init_var( 'options', 'global,type=u,sources=http persistent,default=0,set_scopes=window' );
+
+define( 'OPTION_SHOW_REFERENCES', 0x01 );
 
 
 need_priv( $table, 'read', $any_id );
@@ -24,12 +27,35 @@ open_fieldset( '', "entry: $table / $any_id" );
       foreach( $row as $fieldname => $payload ) {
         open_list_row();
           open_list_cell('fieldname', $fieldname );
-          open_list_cell('payload', any_field_view( $payload, "$fieldname,validate=1" ) );
+          if( $fieldname === "{$table}_id" ) {
+            open_list_cell('payload', "self: $table / $payload", 'dgreen' );
+          } else {
+            open_list_cell('payload', any_field_view( $payload, "$fieldname,validate=1" ) );
+          }
       }
     close_list();
   }
   
 close_fieldset();
+
+if( sql_references( $table, $any_id ) ) {
+  if( $options & OPTION_SHOW_REFERENCES ) {
+    open_fieldset( '', inlink( '', array(
+      'options' => ( $options & ~OPTION_SHOW_REFERENCES )
+    , 'class' => 'close_small'
+    , 'text' => ''
+    ) ) . ' references' );
+      references_view( $table, $any_id );
+    close_fieldset();
+  } else {
+    open_div( 'left smallskipb', inlink( ''
+    , array( 'options' => ( $options | OPTION_SHOW_REFERENCES ) , 'text' => 'references...', 'class' => 'button' )
+    ) );
+  }
+} else {
+  open_div( 'info', '(no references pointing to this entry)' );
+}
+
 
 
 ?>
