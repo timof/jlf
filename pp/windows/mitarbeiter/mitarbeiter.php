e3e9e08c8e86226c06d1961588d29ba8ea5119fe
Timo Felbinger <gittf@qipc.org>
2013-08-27 13:07:59 +0200

    use new menubox css correctly

--- pp/windows/mitarbeiter/mitarbeiter.php
+++ pp/windows/mitarbeiter/mitarbeiter.php
@@ -2,14 +2,16 @@
 
 $f = init_fields( array( 'groups_id' , 'REGEX' => 'size=40,auto=1' ), '' );
 
-open_div('menubox table filters');
-  open_caption( '', filter_reset_button( $f, 'floatright' ) . 'Filter' );
-  open_tr();
-    open_th( '', we('Group:','Bereich:') );
-    open_td( '', filter_group( $f['groups_id'] ) );
-  open_tr();
-    open_th( '', we('Search:','Suche:') );
-    open_td( '', '/'.string_element( $f['REGEX'] ).'/ ' . filter_reset_button( $f['REGEX'] ) );
+open_div('menubox');
+  open_table( 'css');
+    open_caption( '', filter_reset_button( $f, 'floatright' ) . 'Filter' );
+    open_tr();
+      open_th( '', we('Group:','Bereich:') );
+      open_td( '', filter_group( $f['groups_id'] ) );
+    open_tr();
+      open_th( '', we('Search:','Suche:') );
+      open_td( '', '/'.string_element( $f['REGEX'] ).'/ ' . filter_reset_button( $f['REGEX'] ) );
+  close_table();
 close_div();
 
 peoplelist_view( $f['_filters'], 'regex_filter' );
