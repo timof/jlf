ecef88424e539af4dab1c7cc616e4568911bce13
Timo Felbinger <gittf@qipc.org>
2013-08-27 13:01:51 +0200

    yet another instance of degree -> programme

--- pp/windows/forschung/stellen.php
+++ pp/windows/forschung/stellen.php
@@ -1,12 +1,6 @@
 <?php
 
-$f = init_fields( array(
-  'groups_id'
-, 'REGEX' => 'size=40,auto=1'
-, 'degree' => 'u4,min=2012,max=2999,allow_null=0,initval='.$current_year
-), '' );
-
-$f = init_fields( array( 'groups_id', 'degree_id', 'REGEX' => 'size=40,auto=1' ) , '' );
+$f = init_fields( array( 'groups_id', 'programme_id', 'REGEX' => 'size=40,auto=1' ) , '' );
 
 open_div('menubox table filters');
   open_caption( '', filter_reset_button( $f, 'floatright' ) . 'Filter' );
@@ -15,7 +9,7 @@ open_div('menubox table filters');
     open_td( '', filter_group( $f['groups_id'] ) );
   open_tr();
     open_th( '', we('Programme/Degree:','Studiengang/Abschluss:' ) );
-    open_td( '', filter_degree( $f['degree_id'] ) );
+    open_td( '', filter_programme( $f['programme_id'] ) );
   open_tr();
     open_th( '', we('search:','Suche:') );
     open_td( '', '/'.string_element( $f['REGEX'] ).'/ ' . filter_reset_button( $f['REGEX'] ) );
