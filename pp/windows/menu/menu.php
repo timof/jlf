5895e7b8dd095f35bae06ff61adf1dccfa3b92ae
Timo Felbinger <gittf@qipc.org>
2013-09-02 12:57:41 +0200

    start page: use publications views; cosmetics

--- pp/windows/menu/menu.php
+++ pp/windows/menu/menu.php
@@ -6,7 +6,7 @@ echo html_tag( 'a'
     'class' => 'inline_block medskips qquads'
   , 'href' => inlink( 'lehre', 'context=url' )
   )
-, html_div( 'huge bold smallskips a', we('Studying at the Institute','Studium am Institut') )
+, html_div( 'huge bold smallskips a', we('Studies','Studium') )
   . html_tag( 'img', 'width=300,src=/pp/fotos/in_the_lab.jpg' )
 );
 
@@ -17,7 +17,7 @@ echo html_tag( 'a'
     'class' => 'inline_block medskips'
   , 'href' => inlink( 'forschung', 'context=url' )
   )
-, html_div( 'huge bold smallskips', we('Research at the Institute','Forschung am Institut') )
+, html_div( 'huge bold smallskips', we('Research','Forschung') )
   . html_tag( 'img', 'width=300,src=/pp/fotos/in_the_lab.jpg' )
 );
 
@@ -25,9 +25,15 @@ echo html_tag( 'a'
 echo html_tag( 'h2','bigskipt', we('News','Aktuelles') );
 echo html_div( '', inlink( 'aktuelles', 'text='.we('more news...','weitere Meldungen...') ) );
 
-echo html_tag( 'h2','bigskipt', we('Current Publications','Aktuelle Veröffentlichungen') );
-echo publicationsreferenceslist_view();
-echo html_div( '', inlink( 'publikationen', 'text='.we('more publications...','weitere Veröffentlichungen...') ) );
+$publications = sql_publications(
+  'year >= '.( $current_year - 1 )
+, array( 'limit_from' => 1 , 'limit_to' => 3 , 'orderby' => 'year DESC, ctime DESC' )
+);
+if( count( $publications ) >= 2 ) {
+  echo html_tag( 'h2','bigskipt', we('Current Publications','Aktuelle Veröffentlichungen') );
+  echo publication_columns_view( $publications );
+  echo html_div( '', inlink( 'publikationen', 'text='.we('more publications...','weitere Veröffentlichungen...') ) );
+}
 
 
 ?>
