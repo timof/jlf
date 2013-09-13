8260b4a63377df70a11e62f8de683bc915362499
Timo Felbinger <gittf@qipc.org>
2013-09-13 16:37:29 +0200

    new view 'debugentry'; support for table 'debug'

---
+++ code/debugentry.php
@@ -0,0 +1,48 @@
+<?php
+
+$f = init_var( 'debug_id', 'global,type=U,sources=http persistent,set_scopes=self' );
+
+need_priv( 'debug', 'list', $debug_id );
+
+$l = sql_debugentry( $debug_id );
+
+$v = ( have_priv('*','*') ? html_span('qquadl', any_link( 'debug', $debug_id ) ) : '' );
+open_fieldset( 'small_form', "debug entry $v" );
+  open_table( 'hfill td:quads,colgroup=15% 85%' );
+    open_tr();
+      open_td( 'bold', 'id:' );
+      open_td( '', $debug_id );
+    open_tr();
+      open_td( '', 'utc:' );
+      open_td( '', $l['utc'] );
+    open_tr();
+      open_td( '', 'script:' );
+      open_td( '', $l['script'] );
+    open_tr();
+      open_td( '', 'facility:' );
+      open_td( '', $l['facility'] );
+    open_tr();
+      open_td( '', 'object:' );
+      open_td( '', $l['object'] );
+    open_tr();
+      open_td( '', 'comment:' );
+      open_td( '', $l['comment'] );
+
+    open_tr();
+      open_td( 'solidtop bold,colspan=2', 'value:' );
+    open_tr();
+      open_td( 'colspan=2' );
+      open_pre( '', jlf_var_export_html( json_decode( $l['value'], 1 ) ) );
+
+    open_tr();
+      open_td( 'solidtop bold,colspan=2', 'stack:' );
+    open_tr();
+      open_td( 'colspan=2' );
+      open_pre( '', jlf_var_export_html( json_decode( $l['stack'], 1 ) ) );
+
+  close_table();
+
+close_fieldset();
+
+
+?>
