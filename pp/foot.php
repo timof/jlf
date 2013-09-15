1ed18c6de64fcb5839716228ffeef913f4329e1b
Timo Felbinger <gittf@qipc.org>
2013-09-15 12:25:12 +0200

    use debug_window_view() in pp/foot.php

--- pp/foot.php
+++ pp/foot.php
@@ -13,10 +13,7 @@ open_div( 'id=theFooter' );
     open_td( 'right small', "page generated: $now_mysql utc" );
 
   close_table();
-  if( $debug ) {
-    sql_do( 'COMMIT AND CHAIN' );
-    handle_debugging();
-  }
+  debug_window_view();
 close_div();
 
 // insert an invisible submit button to allow to submit the update_form by pressing ENTER:
