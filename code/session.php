0833eb41b47d47607a4b79848d0d789fabfa7a63
Timo Felbinger <gittf@qipc.org>
2013-09-15 10:33:02 +0200

    new script: session.php (wip)

---
+++ code/session.php
@@ -0,0 +1,45 @@
+<?php // /code/session.php
+
+$f = init_var( 'sessions_id', 'global,type=U,sources=http persistent,set_scopes=self' );
+
+need_priv( '*','*' );
+
+sql_transaction_boundary('sessions,people,persistentvars');
+
+$r = sql_one_session( $sessions_id );
+
+open_fieldset( 'small_form', 'session '.any_link( 'sessions', $sessions_id ) );
+
+  open_table( 'hfill td:qquadr;smallskipb,colgroup=10% 90%' );
+    open_tr();
+      open_td( '', 'id:' );
+      open_td( 'bold', $r['sessions_id'] );
+    open_tr();
+      open_td( '', 'person:' );
+      open_td( 'bold', entry_link( 'people', $r['login_people_id'], array( 'text' => $r['cn'] ) ) ); 
+    open_tr();
+      open_td( '', 'authentication method:' );
+      open_td( 'bold', $r['login_authentication_method'] );
+    open_tr();
+      open_td( '', 'ctime:' );
+      open_td( 'bold', $r['ctime'] );
+    open_tr();
+      open_td( '', 'application:' );
+      open_td( 'bold', $r['application'] );
+    open_tr();
+      open_td( '', 'origin:' );
+      open_td( 'bold', "{$r['login_remote_ip']}:{$r['login_remote_port']}" );
+    open_tr();
+      open_td( '', 'atime / valid:' );
+      open_td( 'bold', "{$r['atime']} / {$r['valid']}" );
+    open_tr();
+      open_td( '', 'logbook:' );
+      $n = sql_query( 'logbook', "sessions_id=$sessions_id,single_field=COUNT" );
+      open_td( 'bold', inlink( 'logbook', "sessions_id=$sessions_id,text=$n entries" ) );
+  close_table();
+
+close_fieldset();
+
+persistent_vars_view( "sessions_id=$sessions_id" );
+
+?>
