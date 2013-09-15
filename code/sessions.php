95d8f7f600003bbe6bbe7b371639033a3bb6f59f
Timo Felbinger <gittf@qipc.org>
2013-09-15 10:57:41 +0200

    code/sessions.php: conditional support for groups, affiliations; can now be used in different subprojects

--- code/sessions.php
+++ code/sessions.php
@@ -2,40 +2,61 @@
 
 need_priv('*','*');
 
+sql_transaction_boundary('*');
+
 init_var( 'options', 'global,type=u,sources=http persistent,default=0,set_scopes=window' );
 
 echo html_tag( 'h1', '', "sessions:" );
 
+$have_groups = isset( $tables['groups'] );
+$have_affiliations = ( isset( $tables['affiliations']['cols']['groups_id'] ) && isset( $tables['affiliations']['cols']['people_id'] ) );
 
-$fields = init_fields( array(
-  'REGEX' => 'a,size=40,auto=1'
-//  'people_id' => 'u'
-) );
+$fields =  array( 'REGEX' => 'a,size=40,auto=1' );
+if( function_exists( 'filter_person' ) ) {
+  $fields['people_id'] = 'u';
+  if( $have_groups && function_exists( 'filter_group' ) ) {
+    $fields['groups_id'] = 'u';
+  }
+}
+
+$f = init_fields( $fields );
 
 handle_action( array( 'update' ) );
 switch( $action ) {
   case 'update':
     // nop
     break;
-  case 'deleteLogentry':
-    menatwork();
 }
-
 
 
 open_div('menubox');
   open_table('css filters');
-    open_caption( 'center th', filter_reset_button( $fields, 'floatright' ) . 'Filter' );
-//    open_tr();
-//      open_th( '', 'person:' );
-//      open_td( '', filter_person( $fields['people_id'] ) );
+    open_caption( 'center th', filter_reset_button( $f, 'floatright' ) . 'Filter' );
+if( isset( $f['groups_id'] ) ) {
+    open_tr();
+      open_th( '', 'group:' );
+      open_td( '', filter_group( $f['groups_id'] ) );
+  if( ( $g_id = $f['groups_id']['value'] ) ) {
+    open_tr();
+      open_th( '', 'person:' );
+      open_td( '', filter_person( $f['people_id'], array( 'filters' => "groups_id=$g_id" ) ) );
+  }
+} else if( isset( $f['people_id'] ) ) {
+    open_tr();
+      open_th( '', 'person:' );
+      open_td( '', filter_person( $f['people_id'] ) );
+}
     open_tr();
       open_th( '', 'search:' );
-      open_td( '', string_element( $fields['REGEX'] ) );
+      open_td( '', string_element( $f['REGEX'] ) );
   close_table();
 close_div();
 
+$opts = array();
+if( $have_affiliations ) {
+  $opts['more_joins'] = array( 'affiliations' => 'LEFT affiliations on ( affiliations.people_id = people.people_id )' );
+}
 
-sessions_view( $fields['_filters'] );
+sessions_view( $f['_filters'], $opts );
 
 ?>
