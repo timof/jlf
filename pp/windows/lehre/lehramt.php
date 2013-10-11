8fa08047af1151c45a39370552ae082a90d660d3
Timo Felbinger <gittf@qipc.org>
2013-10-11 16:07:50 +0200

    first version: lehramt.php

--- pp/windows/lehre/lehramt.php
+++ pp/windows/lehre/lehramt.php
@@ -1,134 +1,160 @@
-_m4_dnl <!--  last modified:  20130309.123546utc  by: root@uranos -->
-_m4_include(top.m4)
+<?php
 
+sql_transaction_boundary('*');
 
-_m4_table
-_m4_tr
-<th class='smallskip'>Wahl des Studienortes</th>
-_m4_tr
-_m4_td(class='medskip')
-Das Lehramtsstudium der Physik an der Uni Potsdam besteht aus einem 3-j&auml;hrigen Bachelor und
-einem 2-j&auml;hrigen Masterstudiengang.
-Im Bachelorstudium werden fachliche Kenntnisse im Fach Physik einschlie&szlig;lich der spezifischen
-Erkenntnis- und Arbeitsmethoden sowie Kompetenzen der Fachdidaktik erworben, die dazu bef&auml;higen,
-einen Sch&uuml;ler-orientierten und wissenschaftlich fundierten Physikunterricht zu gestalten.
-Die Ausbildung in experimenteller Physik erfolgt vorwiegend gemeinsam mit Studierenden ohne
-Lehramtsbezug.
+echo html_tag( 'h1', '', we('Bachelor of Education (BEd) Programme','Lehramtsstudium / Bachelor of Education (BEd)' ) );
 
-_m4_dnl
-, wobei &Uuml;bungsgruppen spezifisch auf Fragen der
+echo html_tag( 'h2', '', we('Studying in Potsdam','Wahl des Studienortes Potsdam') );
 
-_m4_tr
-_m4_td
-  _m4_outlink(http://www.uni-potsdam.de/zugang/index.html,[[Einschreibung zum Physikstudium in Potsdam]])
+echo "
+  Das Lehramtsstudium der Physik an der Uni Potsdam besteht aus einem 3-{$aUML}hrigen Bachelor- und
+  einem 2-j{$aUML}hrigen Masterstudiengang.
+  Im Bachelorstudium werden fachliche Kenntnisse im Fach Physik einschlie{$SZLIG}lich der spezifischen
+  Erkenntnis- und Arbeitsmethoden sowie Kompetenzen der Fachdidaktik erworben, die dazu bef{$aUML}higen,
+  einen Sch{$uUML}lerorientierten und wissenschaftlich fundierten Physikunterricht zu gestalten.
+  Die Ausbildung in experimenteller Physik erfolgt vorwiegend gemeinsam mit Studierenden ohne
+  Lehramtsbezug.
+";
 
-_m4_ifelse(1,1,[[
-_m4_tr
-_m4_td
-  _m4_inlink(/lehre/intro.m4,[[
-	  _m4_de(Einf&uuml;hrungsveranstaltungen und Vorkurse)_m4_en(Introductory courses)]],
-		[[_m4_de(Vorbereitende Veranstaltungen vor Beginn des Vorlesungszeitraumes)]])
-]])
-
-_m4_tr
-  _m4_link(
-    /lehre/tutorium.m4,
-    Tutorium f&uuml;r Erstsemester,
-		[[Angebot einer freiwilligen Veranstaltung: Hilfe und Beratung von Studierenden für Studierende]]
+echo tb(
+  html_alink(
+    'http://www.uni-potsdam.de/studium/zugang.html'
+  , 'class=href outlink,text='.we('Immatrikulation for the Phycics BSc/MSc Programme in Potsdam','Einschreibung zum Physikstudium in Potsdam')
   )
+);
 
-_m4_tr
-  _m4_td
-  _m4_outlink([[http://www.uni-potsdam.de/mnfakul/studium/offenermint-raum.html]],
-	  [[MINT-Raum]],[[Lernen mit Hilfe von Kommilitonen]]
-    )
+echo tb( 'Tutorium'
+, inlink( 'tutorium', array( 'text' => we(
+    'Tutorium for beginners: help and guidance from students for students'
+  , "Tutorium f{$uUML}r Studienanf{$aUML}nger: Hilfe und Beratung von Studierenden f{$uUML}r Studierende"
+  ) ) )
+);
 
-_m4_tr
-  _m4_inlink([[/members/persondetails.m4php~p46]],
-	Studienberatung: Dr. Harry Weigt,
-  [[Studienfachberatung f&uuml;r alle Lehramtsstudieng&auml;nge mit Fach Physik]]
-  )
-
-
-_m4_bigskip
-_m4_tr
-<th class='smallskip'>Planung des Studiums</th>
+echo tb( 'Offener MINT Raum'
+, html_alink( 'http://www.uni-potsdam.de/mnfakul/studium/offenermint-raum.html', 'class=href outlink,text=MINT Raum: Lernen mit Hilfe von Kommilitonen' )
+);
 
 
-_m4_smallskip
-_m4_tr
-  _m4_td
-    _m4_p(class='smallskip')
-       Studienverlaufspl&auml;ne
-    _m4_p(class='smallskip' style='padding-left:2em';)
-      _m4_file(/studium/studienverlauf.BEd.Gym1.pdf,[[Bachelor Gymnasium 1.Fach]])
-    _m4_p(class='smallskip' style='padding-left:2em';)
-      _m4_file(/studium/studienverlauf.BEd.Gym2.pdf,[[Bachelor Gymnasium 2.Fach]])
-    _m4_p(class='smallskip' style='padding-left:2em';)
-      _m4_file(/studium/studienverlauf.BEd.SIP1.pdf,[[Bachelor SIP 1.Fach]])
-    _m4_p(class='smallskip' style='padding-left:2em';)
-      _m4_file(/studium/studienverlauf.BEd.SIP2.pdf,[[Bachelor SIP 2.Fach]])
-    _m4_p(class='smallskip' style='padding-left:2em';)
-      _m4_file(/studium/studienverlauf.MEd.Gym1.pdf,[[Master Gymnasium 1.Fach]])
-    _m4_p(class='smallskip' style='padding-left:2em';)
-      _m4_file(/studium/studienverlauf.MEd.Gym2.pdf,[[Master Gymnasium 2.Fach]])
-    _m4_p(class='smallskip' style='padding-left:2em';)
-      _m4_file(/studium/studienverlauf.MEd.SIP1.pdf,[[Master SIP 1.Fach]])
-    _m4_p(class='smallskip' style='padding-left:2em';)
-      _m4_file(/studium/studienverlauf.MEd.SIP2.pdf,[[Master SIP 2.Fach]])
+// echo tb( we('Introductory courses',"Einf{$uUML}hrungsveranstaltungen und Vorkurse")
+// , inlink( 'intro', array( 'text' => we('Introductory courses for beginners',"Einf{$uUML}hrungsveranstaltungen und Vorkurse vor Beginn des Vorlesungszeitraums") ) )
+// );
 
-_m4_smallskip
-
-_m4_ifelse([[
-_m4_tr
-  _m4_td
-  _m4_file(lehramt.uebersicht.pdf,Veranstaltungsübersicht)
-]])
-
-_m4_tr
-  _m4_file(
-    /studium/Handbuch_LAPhysik_111006.pdf,
-    [[Modulhandbuch Lehramt Physik (Fassung vom 06.10.2011)]]
-  )
-_m4_tr
-  _m4_file(
-    http://theosolid.qipc.org/KomVV_WS2012.pdf,
-    [[_m4_de([[Kommentiertes Vorlesungsverzeichnis: Physik, Wintersemester 2012/13]])_m4_en([[Course Catalog: Physics, Winter term 2012/13]])]]
-  )
-_m4_tr
-_m4_td
-  _m4_inlink(/lehre/belegung.m4,[[Belegen von Lehrveranstaltungen und Anmeldung zu Pr&uuml;fungen]])
-_m4_tr
-  _m4_td
-  _m4_link(/lehre/termine.m4,Wichtige Termine f&uuml;r Studierende am Institut)
+echo tb( we('Course guidance for students in BEd and MEd programme',"Studienfachberatung Physik f{$uUML}r Studierende im Lehramtsstudium (BEd umd MEd)")
+       , alink_person_view( 'people_id!=0,board=guidance,function=lehramt', 'office=1,format=list' )
+);
 
 
-_m4_medskip
-_m4_tr
-  _m4_file(
-    /studium/BAMALAPHYS_2011_amtlich.pdf,
-    [[Studienordnung Bachelor/Master Physik Lehramt (2011)]]
-  )
-  _m4_p(style='padding-left:2em;')
-  _m4_inlink(/lehre/studienordnungen.m4,[[&auml;ltere Fassungen...]])
-  _m4_ap
+echo html_tag( 'h2', 'medskipt', we('Planning your studies','Planung des Studiums') );
 
 
-_m4_medskip
-_m4_tr
-  _m4_td
-  _m4_inlink(/lehre/themen.bachelor.m4,[[Themenvorschl&auml;ge f&uuml;r Bachelorarbeiten]])
+echo tb( we('Programme schedule','Studienverlaufsplan' )
+       , alink_document_view( array( 'type' => 'SVP', 'programme_id &=' => PROGRAMME_BED ), 'format=latest' )
+);
 
-_m4_tr
-  _m4_td
-  _m4_inlink(/lehre/themen.master.m4,[[Themenvorschl&auml;ge f&uuml;r Masterarbeiten]])
+echo tb( we('Module manual BSc',"Modulhandbuch BSc")
+       , alink_document_view( array( 'type' => 'MHB', 'programme_id &=' => PROGRAMME_BED ), 'format=latest' )
+);
+
+echo tb( we('Course regulations BSc',"Studienordnung BSc")
+       , alink_document_view( array( 'type' => 'SO', 'programme_id &=' => PROGRAMME_BED ), 'format=latest' )
+);
+
+echo tb( we('Course overview',"Veranstaltungs{$uUML}bersicht")
+       , alink_document_view( array( 'type' => 'VUeS', 'programme_id &=' => PROGRAMME_BED ), 'format=latest' )
+);
+
+echo tb( we('Course catalog',"Vorlesungsverzeichnis")
+       , alink_document_view( array( 'type' => 'VVZ' ), 'format=latest' )
+);
+
+echo tb( we('Registration for courses and examinations',"Anmeldung zu Veranstaltungen und Pr{$uUML}fungen" )
+ , html_alink( 'http://puls.uni-potsdam.de', array( 'class' => 'href outlink', 'text' => we('Online portal: PULS','Online-Portal: PULS') ) )
+);
 
 
+echo tb( we('Bachelor Theses','Bachelor Arbeiten')
+ , array(
+     inlink( 'themen', array( 'programme_id' => PROGRAMME_BED, 'text' => we('Topics for Bachelor Theses',"Themenvorschl{$aUML}ge f{$uUML}r Bachelorarbeiten") ) )
+   , inlink( 'themen', array( 'programme_id' => PROGRAMME_MED, 'text' => we('Topics for Master Theses',"Themenvorschl{$aUML}ge f{$uUML}r Masterarbeiten") ) )
+   )
+);
 
+// _m4_smallskip
+// _m4_tr
+//   _m4_td
+//     _m4_p(class='smallskip')
+//        Studienverlaufspl&auml;ne
+//     _m4_p(class='smallskip' style='padding-left:2em';)
+//       _m4_file(/studium/studienverlauf.BEd.Gym1.pdf,[[Bachelor Gymnasium 1.Fach]])
+//     _m4_p(class='smallskip' style='padding-left:2em';)
+//       _m4_file(/studium/studienverlauf.BEd.Gym2.pdf,[[Bachelor Gymnasium 2.Fach]])
+//     _m4_p(class='smallskip' style='padding-left:2em';)
+//       _m4_file(/studium/studienverlauf.BEd.SIP1.pdf,[[Bachelor SIP 1.Fach]])
+//     _m4_p(class='smallskip' style='padding-left:2em';)
+//       _m4_file(/studium/studienverlauf.BEd.SIP2.pdf,[[Bachelor SIP 2.Fach]])
+//     _m4_p(class='smallskip' style='padding-left:2em';)
+//       _m4_file(/studium/studienverlauf.MEd.Gym1.pdf,[[Master Gymnasium 1.Fach]])
+//     _m4_p(class='smallskip' style='padding-left:2em';)
+//       _m4_file(/studium/studienverlauf.MEd.Gym2.pdf,[[Master Gymnasium 2.Fach]])
+//     _m4_p(class='smallskip' style='padding-left:2em';)
+//       _m4_file(/studium/studienverlauf.MEd.SIP1.pdf,[[Master SIP 1.Fach]])
+//     _m4_p(class='smallskip' style='padding-left:2em';)
+//       _m4_file(/studium/studienverlauf.MEd.SIP2.pdf,[[Master SIP 2.Fach]])
+// 
+// _m4_smallskip
+// 
+// _m4_ifelse([[
+// _m4_tr
+//   _m4_td
+//   _m4_file(lehramt.uebersicht.pdf,Veranstaltungsübersicht)
+// ]])
+// 
+// _m4_tr
+//   _m4_file(
+//     /studium/Handbuch_LAPhysik_111006.pdf,
+//     [[Modulhandbuch Lehramt Physik (Fassung vom 06.10.2011)]]
+//   )
+// _m4_tr
+//   _m4_file(
+//     http://theosolid.qipc.org/KomVV_WS2012.pdf,
+//     [[_m4_de([[Kommentiertes Vorlesungsverzeichnis: Physik, Wintersemester 2012/13]])_m4_en([[Course Catalog: Physics, Winter term 2012/13]])]]
+//   )
+// _m4_tr
+// _m4_td
+//   _m4_inlink(/lehre/belegung.m4,[[Belegen von Lehrveranstaltungen und Anmeldung zu Pr&uuml;fungen]])
+// _m4_tr
+//   _m4_td
+//   _m4_link(/lehre/termine.m4,Wichtige Termine f&uuml;r Studierende am Institut)
+// 
+// 
+// _m4_medskip
+// _m4_tr
+//   _m4_file(
+//     /studium/BAMALAPHYS_2011_amtlich.pdf,
+//     [[Studienordnung Bachelor/Master Physik Lehramt (2011)]]
+//   )
+//   _m4_p(style='padding-left:2em;')
+//   _m4_inlink(/lehre/studienordnungen.m4,[[&auml;ltere Fassungen...]])
+//   _m4_ap
+// 
+// 
+// _m4_medskip
+// _m4_tr
+//   _m4_td
+//   _m4_inlink(/lehre/themen.bachelor.m4,[[Themenvorschl&auml;ge f&uuml;r Bachelorarbeiten]])
+// 
+// _m4_tr
+//   _m4_td
+//   _m4_inlink(/lehre/themen.master.m4,[[Themenvorschl&auml;ge f&uuml;r Masterarbeiten]])
+// 
+// 
+// 
+// 
+// 
+// _m4_bigskip
+// _m4_atable
+// 
+// _m4_include(bottom.m4)
+// 
 
-
-_m4_bigskip
-_m4_atable
-
-_m4_include(bottom.m4)
-
+?>
