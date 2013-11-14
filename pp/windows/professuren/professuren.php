87940a678c62b6cb5561d81a14aa82338ad40f71
Timo Felbinger <gittf@qipc.org>
2013-11-14 13:35:42 +0100

    list new professors

--- pp/windows/professuren/professuren.php
+++ pp/windows/professuren/professuren.php
@@ -13,29 +13,50 @@ open_ul('plain');
   }
 close_ul('plain');
 
-open_tag('h2', '', we('Auxiliary Professors:',"Au{$SZLIG}erplanm{$aUML}{$SZLIG}ige Professuren:") );
-$profs = sql_people( array( 'flag_publish', 'flag_deleted=0', 'flag_virtual=0', 'status' => PEOPLE_STATUS_SPECIAL ) );
-open_ul('plain');
-  foreach( $profs as $p ) {
-    open_li( '', alink_person_view( $p['people_id'], 'showgroup=1' ) );
-  }
-close_ul('plain');
+if( ( $profs = sql_people( array( 'flag_publish', 'flag_deleted=0', 'flag_virtual=0', 'status' => PEOPLE_STATUS_SPECIAL ) ) ) ) {
+  open_tag('h2', '', we('Auxiliary Professors:',"Au{$SZLIG}erplanm{$aUML}{$SZLIG}ige Professuren:") );
+  open_ul('plain');
+    foreach( $profs as $p ) {
+      open_li( '', alink_person_view( $p['people_id'], 'showgroup=1' ) );
+    }
+  close_ul('plain');
+}
 
-open_tag('h2', '', we('Jointly Appointed Professors:',"Gemeinsam berufene Professuren:") );
-$profs = sql_people( array( 'flag_publish', 'flag_deleted=0', 'flag_virtual=0', 'status' => PEOPLE_STATUS_JOINT ) );
-open_ul('plain');
-  foreach( $profs as $p ) {
-    open_li( '', alink_person_view( $p['people_id'], 'showgroup=1' ) );
-  }
-close_ul('plain');
+if( ( $profs = sql_people( array( 'flag_publish', 'flag_deleted=0', 'flag_virtual=0', 'status' => PEOPLE_STATUS_JOINT ) ) ) ) {
+  open_tag('h2', '', we('Jointly Appointed Professors:',"Gemeinsam berufene Professuren:") );
+  open_ul('plain');
+    foreach( $profs as $p ) {
+      open_li( '', alink_person_view( $p['people_id'], 'showgroup=1' ) );
+    }
+  close_ul('plain');
+}
 
-open_tag('h2', '', we('External Professors:',"Externe Professuren:") );
-$profs = sql_people( array( 'flag_publish', 'flag_deleted=0', 'flag_virtual=0', 'status' => PEOPLE_STATUS_EXTERNAL ) );
-open_ul('plain');
-  foreach( $profs as $p ) {
-    open_li( '', alink_person_view( $p['people_id'], 'showgroup=1' ) );
-  }
-close_ul('plain');
+if( ( $profs = sql_people( array( 'flag_publish', 'flag_deleted=0', 'flag_virtual=0', 'status' => PEOPLE_STATUS_HONORARY ) ) ) ) {
+  open_tag('h2', '', we('Honorary Professors:',"Honorarprofessuren:") );
+  open_ul('plain');
+    foreach( $profs as $p ) {
+      open_li( '', alink_person_view( $p['people_id'], 'showgroup=1' ) );
+    }
+  close_ul('plain');
+}
+
+if( ( $profs = sql_people( array( 'flag_publish', 'flag_deleted=0', 'flag_virtual=0', 'status' => PEOPLE_STATUS_EXTERNAL ) ) ) ) {
+  open_tag('h2', '', we('External Professors:',"Externe Professuren:") );
+  open_ul('plain');
+    foreach( $profs as $p ) {
+      open_li( '', alink_person_view( $p['people_id'], 'showgroup=1' ) );
+    }
+  close_ul('plain');
+}
+
+if( ( $profs = sql_people( array( 'flag_publish', 'flag_deleted=0', 'flag_virtual=0', 'status' => PEOPLE_STATUS_SENIOR ) ) ) ) {
+  open_tag('h2', '', we('Senior Academic Assistants:',"Privatdozenten:") );
+  open_ul('plain');
+    foreach( $profs as $p ) {
+      open_li( '', alink_person_view( $p['people_id'], 'showgroup=1' ) );
+    }
+  close_ul('plain');
+}
 
 
 ?>
