0ff86d48302ddbced5aa3ac294893289a5128d07
Timo Felbinger <gittf@qipc.org>
2013-11-24 11:57:52 +0100

    filters for events

--- pp/windows/institut/veranstaltung.php
+++ pp/windows/institut/veranstaltung.php
@@ -4,37 +4,35 @@ sql_transaction_boundary('*');
 
 init_var( 'events_id', 'global,type=U6,sources=http self,set_scopes=self url' );
 
-if( ! $event = sql_one_event( "events_id=$events_id", 0 ) ) {
-  open_div( 'warn', 'query failed - no such topic or position' );
+if( ! ( $event = sql_one_event( "events_id=$events_id,flag_publish", 0 ) ) ) {
+  open_div( 'warn', 'query failed - no such event' );
   return;
 }
 
 if( $deliverable ) switch( $deliverable ) {
 
   case 'event':
-//     $event = array(
-//       'dn' => "positions_id=$positions_id,ou=positions,ou=physik,o=uni-potsdam,c=de"
-//     , 'cn' => $position['cn']
+     $event = array(
+//       'dn' => "events_id=$events_id,ou=events,ou=physik,o=uni-potsdam,c=de"
+         'cn' => $event['cn']
 //     , 'programme_cn' => $position['programme_cn']
 //     , 'groups_cn' => $position['groups_cn']
 //     , 'people_cn' => $position['people_cn']
-//     , 'url' => $position['url']
-//     , 'note' => $position['note']
-//     );
-//     switch( $global_format ) {
+     , 'url' => $event['url']
+     , 'note' => $event['note']
+     );
+     switch( $global_format ) {
 //       case 'pdf':
 //         begin_deliverable( 'position', 'pdf'
 //         , tex2pdf( 'position.tex', array( 'loadfile', 'row' => $position ) )
 //         );
 //         break;
-//       case 'ldif':
-//         begin_deliverable( 'position', 'ldif'
-//         , ldif_encode( $position )
-//         );
-//         break;
-//       default:
-//         error( "unsupported format: [$global_format]" );
-//     }
+       case 'ldif':
+         begin_deliverable( 'event', 'ldif' , ldif_encode( $event ) );
+         break;
+       default:
+         error( "unsupported format: [$global_format]" );
+     }
     return;
 
   default:
