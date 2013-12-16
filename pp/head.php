aea55189d4c5e1590703b4efa05443929caa2bec
Timo Felbinger <gittf@qipc.org>
2013-12-16 07:57:37 +0100

    make upper left corner image clickable

--- pp/head.php
+++ pp/head.php
@@ -26,7 +26,9 @@ require_once( 'pp/media.php' );
 require_once( 'pp/outlinks.php' );
 
 open_div( 'hfill,id=theHeader' );
-  open_div( 'id=theLeftLogo', html_tag( 'img', 'src=/pp/img/haus28innen.gif,alt='.we('physics institute - inside view','Physikinstitut - Innenansicht'), NULL ) );
+  $l = html_tag( 'img', 'src=/pp/img/haus28innen.gif,alt='.we('physics institute - inside view','Physikinstitut - Innenansicht'), NULL );
+  $l = inlink( 'menu', array( 'class' => 'image', 'text' => $l ) );
+  open_div( 'id=theLeftLogo', $l );
   open_div( 'id=theFaknav' );
     require_once( 'pp/faknav.php' );
   close_div();
