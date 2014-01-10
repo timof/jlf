848b85f1c5419fd61a86c620393ef0a06b71178f
Timo Felbinger <gittf@qipc.org>
2014-01-10 17:15:00 +0100

    more info on enrollment and application

--- pp/windows/lehre/einschreibung.php
+++ pp/windows/lehre/einschreibung.php
@@ -7,11 +7,11 @@ echo html_tag( 'h1', '', we('Enrollment for Courses in Physics',"Einschreibung z
 echo tb( we('Contact and guidance for prospective students:',"Ansprechpartner und Beratung zu allen Fragen zur Einschreibung:")
        , alink_person_view( 'people_id!=0,board=guidance,function=enrollment', 'office=1,format=list' )
 );
-echo tb( we(" You can apply (only required for the Master programme) or enroll on the web site of the University: "
-           ,"Bewerbung (nur erforderlich f{$uUML}r Master-Studieng{$aUML}nge) und Einschreibung erfolgen {$uUML}ber die Webseite der Universit{$aUML}t:" )
+echo tb( we(" You can apply (in physics, only required for the Master programme) or enroll on the web site of the University: "
+           ,"Bewerbung (im Fach Physik nur erforderlich f{$uUML}r Master-Studieng{$aUML}nge) und Einschreibung erfolgen {$uUML}ber die Webseite der Universit{$aUML}t:" )
 , array(
     html_alink(
-      'http://www.uni-potsdam.de/studium/zugang.html'
+      we('http://www.uni-potsdam.de/studium/zugang0.html', 'http://www.uni-potsdam.de/studium/zugang.html')
     , 'class=href outlink,text='.we('Application and Enrollment at the University of Potsdam','Bewerbung und Einschreibung an der Universität Potsdam')
     ) 
   , we(' (note that enrollment will only be possible in the periods specified above!)', " (Einschreibung ist nur in den unten angegebenen Zeitr{$aUML}umen m{$oUML}glich!)" )
@@ -25,7 +25,7 @@ open_tag( 'p', 'smallskips', we(
  required before enrollment.
 ", "
  Die Bachelor-Studiengaenge BSc (Bachelor of Science, Ein-Fach-Bachelor) und BEd (Bachelor of Education, Lehramt mit Fach Physik)
- sind nicht zulassungsbeschränkt; die Einschreibung erfolgt ohne vorherige Bewerbung
+ sind nicht zulassungsbeschr{$aUML}nkt; die Einschreibung erfolgt ohne vorherige Bewerbung.
 ") );
 
 open_tag( 'p', 'smallskips', we(
@@ -37,6 +37,8 @@ open_tag( 'p', 'smallskips', we(
  Für höhere Fachsemester (alle außer dem ersten Fachsemester) ist die Einschreibung auch vom 15.02. bis 15.03 für das folgende Sommersemester möglich.
 ") );
 
+open_tag( 'p', 'smallskips', we( "Enrollment is only possible in the specified periods.", "Die Einschreibung ist nur in den angegebenen Zeitr{$aUML}umen m{$oUML}glich." ) );
+
 echo tb( we('Contact and guidance for the BSc programme:',"Ansprechpartner und Beratung zum BSc-Studiengang (Ein-Fach-Bachelor):")
        , alink_person_view( 'people_id!=0,board=guidance,function=mono', 'office=1,format=list' )
 );
@@ -46,6 +48,13 @@ echo tb( we('Contact and guidance for the BEd programme:',"Ansprechpartner und B
 
 
 echo html_tag( 'h2', 'medskipt', we('Application for the MSc and MEd programme','Bewerbung zum Masterstudium (MSc oder MEd)') );
+
+echo tb( we("Enrollment in the Master programme requires application for admission:", "Vor der Einschreibung zum Masterstudium ist eine Bewerbung um Zulassung erforderlich:")
+, html_alink(
+      we('http://www.uni-potsdam.de/studium/zugang0/application-master.html', 'http://www.uni-potsdam.de/studium/zugang/bewerbung-master.html')
+    , 'class=href outlink,text='.we('Application for the Master programme at the University of Potsdam', "Bewerbung zum Masterstudiunm an der Universit{$aUML}t Potsdam" )
+    ) 
+);
 
 echo tb( we('Contact and guidance for the MSc programme:',"Ansprechpartner und Beratung zum MSc-Studiengang (Ein-Fach-Master):")
        , alink_person_view( 'people_id!=0,board=guidance,function=mono', 'office=1,format=list' )
