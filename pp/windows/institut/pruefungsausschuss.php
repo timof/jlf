7c1c87cbd5e90e629d7ace24fffdb0dc12d9a367
Timo Felbinger <gittf@qipc.org>
2014-09-09 11:10:16 +0200

    cosmetics

--- pp/windows/institut/pruefungsausschuss.php
+++ pp/windows/institut/pruefungsausschuss.php
@@ -16,14 +16,14 @@ foreach( array( 'examBoardMono' , 'examBoardEdu', 'studiesBoard' ) as $boardname
         if( $fname[ 0 ] === '_' )
           continue;
         open_tr();
-          open_th( 'left,colspan=3', $function['function'] );
+          open_th( 'left,colspan=4', $function['function'] );
           $members = sql_offices( "board=$boardname,function=$fname", 'orderby=rank' );
           foreach( $members as $m ) {
             open_tr();
               open_td( '', alink_person_view( $m['people_id'], 'office' ) );
               open_td( '', $m['roomnumber'] );
               open_td( '', $m['telephonenumber'] );
-              open_td( '', $m['office_hours'] );
+              open_td( 'style=max-width:200px;', $m['office_hours'] );
           }
       }
     close_table();
