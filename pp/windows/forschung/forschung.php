cdb9a45c624d97fb759b40f4e91138ce4033fd5c
Timo Felbinger <gittf@qipc.org>
2015-05-26 13:07:37 +0200

    fixes

--- pp/windows/forschung/forschung.php
+++ pp/windows/forschung/forschung.php
@@ -60,12 +60,15 @@ function schwerpunkt( $topic, $title, $image_view, $text, $modules = array() ) {
         open_tag('h3', '', we('Honorary Professors:','Honorarprofessuren:') );
         open_ul('plain');
         foreach( $profs as $p ) {
-          $more = '';
-          // if( $p['acronym'] == 'exatp' ) {
-          //   if( ( $person = sql_person( 'cn=christian stegmann', 'default=0' ) ) ) {
-          //     $more = "showmore={$person['people_id']},";
-          //   }
-          // }
+          open_li( '', alink_person_view( $p['people_id'], "fullname=1" ) );
+        }
+        close_ul();
+      }
+
+      if( ( $profs = sql_people( array( 'flag_publish', 'keyarea' => $topic, 'status' => PEOPLE_STATUS_EMERITUS ) ) ) ) {
+        open_tag('h3', '', we('Emeriti:','Emeritierte:') );
+        open_ul('plain');
+        foreach( $profs as $p ) {
           open_li( '', alink_person_view( $p['people_id'], "fullname=1" ) );
         }
         close_ul();
