37690ad505b3ee65ed08f82261d915d432d00de1
Timo Felbinger <gittf@qipc.org>
2016-03-09 15:30:42 +0100

    cosmetics

--- pp/windows/lehre/praktika.php
+++ pp/windows/lehre/praktika.php
@@ -8,10 +8,12 @@ foreach( array( 'gp', 'fp' ) as $acronym ) {
   if( ( $group = sql_one_group( "acronym=$acronym,flag_publish", 0 ) ) ) {
     $groups_id = $group['groups_id'];
 
-    echo group_view( $group );
-    echo html_tag( 'h3', '', we('Members:','Mitglieder:') );
-
-    peoplelist_view( "groups_id=$groups_id", 'columns=groups=t=0,select=1,insert=1' );
+    open_div( 'medpads' );
+      echo group_view( $group, 'hlevel=2' );
+      echo html_tag( 'h3', '', we('Members:','Mitglieder:') );
+  
+      peoplelist_view( "groups_id=$groups_id", 'columns=groups=t=0,select=1,insert=1' );
+    close_div();
   }
 }
 
