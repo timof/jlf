c134fff012103523d02817ef5d8da40bd8d7b6a9
Timo Felbinger <gittf@qipc.org>
2016-03-10 13:06:04 +0100

    simplify lab course list

--- pp/windows/lehre/praktika.php
+++ pp/windows/lehre/praktika.php
@@ -4,19 +4,17 @@ sql_transaction_boundary('*');
 
 echo html_tag( 'h1', '', we('Lab Courses:','Praktika am Institut:') );
 
-foreach( array( 'gp', 'fp' ) as $acronym ) {
-  if( ( $group = sql_one_group( "acronym=$acronym,flag_publish", 0 ) ) ) {
-    $groups_id = $group['groups_id'];
+$groups = sql_groups( array( 'status' => GROUPS_STATUS_LABCOURSE, 'flag_publish' ), "orderby=acronym DESC" );
+foreach( $groups as $group ) {
+  $groups_id = $group['groups_id'];
 
-    open_div( 'medpads' );
-      echo group_view( $group, 'hlevel=2' );
-      echo html_tag( 'h3', '', we('Members:','Mitglieder:') );
-  
-      peoplelist_view( "groups_id=$groups_id", 'columns=groups=t=0,select=1,insert=1' );
-    close_div();
-  }
+  open_div( 'medpads' );
+    echo group_view( $group, 'hlevel=2' );
+    echo html_tag( 'h3', 'noskipt nopadt', we('Members:','Mitglieder:') );
+
+    peoplelist_view( "groups_id=$groups_id", 'columns=groups=t=0,select=1,insert=1' );
+  close_div();
 }
-
 
 
 ?>
