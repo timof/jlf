ded9987e6627fe7525d70860daf254d26b4ab486
Timo Felbinger <gittf@qipc.org>
2016-03-10 11:10:33 +0100

    fix examiners list

--- deploy.sh
+++ deploy.sh
@@ -17,7 +17,6 @@ chmod 755 .
 chmod 600 ./.gitignore
 chmod 600 ./README
 chmod 755 ./alien
-chmod 644 ./alien/prototype.js
 chmod 755 ./cluster
 chmod 600 ./cluster/1structure.php
 chmod 644 ./cluster/basic.php
@@ -391,7 +390,6 @@ chmod 644 ./pi/gadgets.php
 chmod 644 ./pi/garbage.php
 chmod 644 ./pi/html.php
 chmod 644 ./pi/inlinks.php
-chmod 644 ./pi/maintenance.php
 chmod 755 ./pi/public
 chmod 755 ./pi/shared
 chmod 644 ./pi/shared/basic.php
