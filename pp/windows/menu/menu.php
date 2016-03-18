76509b3350d055a11b6363bc74594c1d212eb13b
Timo Felbinger <gittf@qipc.org>
2016-03-18 12:45:22 +0100

    cosmetics

--- pp/windows/menu/menu.php
+++ pp/windows/menu/menu.php
@@ -53,7 +53,7 @@ foreach( $highlights as $r ) {
   $items[] = highlight_view( $r, 'format=highlight' );
 }
 if( $items ) {
-  open_div( 'id=tickerbox,medskips' );
+  open_div( 'ccbox medskips' );
     echo html_tag( 'h2','', we('Highlights','Aus dem Institut') );
     foreach( $items as $r ) {
       echo $r;
