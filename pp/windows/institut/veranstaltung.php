65b0061230d4b478f97f030c3050e61f7b4e9e64
Timo Felbinger <gittf@qipc.org>
2016-12-12 11:18:51 +0100

    special kludges

--- pp/windows/institut/veranstaltung.php
+++ pp/windows/institut/veranstaltung.php
@@ -48,6 +48,76 @@ if( $deliverable ) switch( $deliverable ) {
     error("no such deliverable: $deliverable");
 }
 
-echo event_view( $event, 'format=detail' );
+if( $events_id == 79 ) {
+
+
+      echo html_tag( "h1", '', 'Verleihung des Carl-Ramsauer-Preises 2016' );
+      
+      echo html_tag( 'img', 'class=floatright,width=400px,margin=1em,src=/pp/fotos/ramsauer/r29.jpg' );
+
+      echo "Am 23. November wurde der Carl-Ramsauer-Preises der Physikalischen
+           Gesellschaft zu Berlin für die besten Doktorarbeiten in
+           Physik an der Humboldt-Universität, Freien Universität,
+           Technischen Universität und Universität Potsdam verliehen. 
+           Hier finden Sie einige Impressionen von der Veranstaltung 
+           (Fotos: Daniela Höpfner).
+           ";
+
+     echo html_div( 'clear', '' );
+     echo html_tag( 'img', 'width=600px,medpads,src=/pp/fotos/ramsauer/r01.jpg' );
+     echo html_tag( 'img', 'width=600px,medpads,src=/pp/fotos/ramsauer/r02.jpg' );
+     echo html_tag( 'img', 'width=600px,medpads,src=/pp/fotos/ramsauer/r03.jpg' );
+     echo html_tag( 'img', 'width=600px,medpads,src=/pp/fotos/ramsauer/r04.jpg' );
+     echo html_tag( 'img', 'width=600px,medpads,src=/pp/fotos/ramsauer/r05.jpg' );
+     echo html_tag( 'img', 'width=600px,medpads,src=/pp/fotos/ramsauer/r06.jpg' );
+     echo html_tag( 'img', 'width=600px,medpads,src=/pp/fotos/ramsauer/r07.jpg' );
+     echo html_tag( 'img', 'width=600px,medpads,src=/pp/fotos/ramsauer/r08.jpg' );
+     echo html_tag( 'img', 'width=600px,medpads,src=/pp/fotos/ramsauer/r09.jpg' );
+
+     echo html_tag( 'img', 'width=600px,medpads,src=/pp/fotos/ramsauer/r10.jpg' );
+     echo html_tag( 'img', 'width=600px,medpads,src=/pp/fotos/ramsauer/r11.jpg' );
+     echo html_tag( 'img', 'width=600px,medpads,src=/pp/fotos/ramsauer/r12.jpg' );
+     echo html_tag( 'img', 'width=600px,medpads,src=/pp/fotos/ramsauer/r13.jpg' );
+     echo html_tag( 'img', 'width=600px,medpads,src=/pp/fotos/ramsauer/r14.jpg' );
+     echo html_tag( 'img', 'width=600px,medpads,src=/pp/fotos/ramsauer/r15.jpg' );
+     echo html_tag( 'img', 'width=600px,medpads,src=/pp/fotos/ramsauer/r16.jpg' );
+     echo html_tag( 'img', 'width=600px,medpads,src=/pp/fotos/ramsauer/r17.jpg' );
+     echo html_tag( 'img', 'width=600px,medpads,src=/pp/fotos/ramsauer/r18.jpg' );
+     echo html_tag( 'img', 'width=600px,medpads,src=/pp/fotos/ramsauer/r19.jpg' );
+
+     echo html_tag( 'img', 'width=600px,medpads,src=/pp/fotos/ramsauer/r20.jpg' );
+     echo html_tag( 'img', 'width=600px,medpads,src=/pp/fotos/ramsauer/r21.jpg' );
+     echo html_tag( 'img', 'width=600px,medpads,src=/pp/fotos/ramsauer/r22.jpg' );
+     echo html_tag( 'img', 'width=600px,medpads,src=/pp/fotos/ramsauer/r23.jpg' );
+     echo html_tag( 'img', 'width=600px,medpads,src=/pp/fotos/ramsauer/r24.jpg' );
+     echo html_tag( 'img', 'width=600px,medpads,src=/pp/fotos/ramsauer/r25.jpg' );
+     echo html_tag( 'img', 'width=600px,medpads,src=/pp/fotos/ramsauer/r26.jpg' );
+     echo html_tag( 'img', 'width=600px,medpads,src=/pp/fotos/ramsauer/r27.jpg' );
+     echo html_tag( 'img', 'width=600px,medpads,src=/pp/fotos/ramsauer/r28.jpg' );
+     echo html_tag( 'img', 'width=600px,medpads,src=/pp/fotos/ramsauer/r29.jpg' );
+     
+     echo html_tag( 'img', 'width=600px,medpads,src=/pp/fotos/ramsauer/r30.jpg' );
+     echo html_tag( 'img', 'width=600px,medpads,src=/pp/fotos/ramsauer/r31.jpg' );
+     echo html_tag( 'img', 'width=600px,medpads,src=/pp/fotos/ramsauer/r32.jpg' );
+
+} else if( $events_id == 81 ) {
+
+
+     echo html_tag( "h1", '', 'Absolventenverabschiedung 2016' );
+
+     echo html_tag( 'img', 'class=floatright,width=400px,margin=1em,src=/pp/fotos/abs2016/abs2016e.jpg' );
+
+     echo "Einige Impressionen von der Feier zur Verabschiedung der diesjährigen Absolventen am 09.12.2016 (Fotos: Achim Feldmeier)";
+
+     echo html_div( 'clear', '' );
+     echo html_tag( 'img', 'width=600px,medpads,src=/pp/fotos/abs2016/abs2016a.jpg' );
+     echo html_tag( 'img', 'width=600px,medpads,src=/pp/fotos/abs2016/abs2016b.jpg' );
+     echo html_tag( 'img', 'width=600px,medpads,src=/pp/fotos/abs2016/abs2016c.jpg' );
+     echo html_tag( 'img', 'width=600px,medpads,src=/pp/fotos/abs2016/abs2016d.jpg' );
+     echo html_tag( 'img', 'width=600px,medpads,src=/pp/fotos/abs2016/abs2016e.jpg' );
+
+} else { 
+  echo event_view( $event, 'format=detail' );
+}
 
 ?>
