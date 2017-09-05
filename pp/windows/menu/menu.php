84533e93805e7295873a1b69cf1ef10f52f368cb
Timo Felbinger <gittf@qipc.org>
2017-09-05 14:57:39 +0200

    publish current VVZ

--- pp/windows/menu/menu.php
+++ pp/windows/menu/menu.php
@@ -34,7 +34,7 @@ foreach( $events as $r ) {
 }
 // $items[] = html_span( 'tickerline', inlink( 'tutorium', 'text='.we('Tutorial in Summer term 2017','Tutorium im Sommersemester 2017') ) );
 $items[] = html_span( 'tickerline', inlink( 'einschreibung', array( 'text' => we('Information for prospective students', "Informationen f{$uUML}r Studieninteressierte" ) ) ) );
-// $items[] = html_span( 'tickerline', alink_document_view( 'type=VVZ,flag_current', 'format=latest' ) );
+$items[] = html_span( 'tickerline', alink_document_view( 'type=VVZ,flag_current', 'format=latest' ) );
 open_div( 'id=tickerbox,medskips' );
   echo html_tag( 'h2','', we('News','Aktuelles') );
 
