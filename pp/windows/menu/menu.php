78eb6bf9f83495f8ea055169d06f3ccf5cc828b9
Timo Felbinger <gittf@qipc.org>
2017-09-27 08:47:55 +0200

    typo

--- pp/windows/menu/menu.php
+++ pp/windows/menu/menu.php
@@ -7,7 +7,7 @@ open_div('id=teaser');
   open_div( 'class=overlay,id=i1', image('lehre') );
   open_div( 'class=overlay,id=i2', image('forschung') );
 
-  open_div('id=linkbox');
+  open_div('linkbox');
     echo inlink( 'lehre', array(
       'class' => 'link'
     , 'text' => we('Studies','Studium')
