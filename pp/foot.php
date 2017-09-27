bd75b2029bb17c60d45c986fb1594d18872ffa54
Timo Felbinger <gittf@qipc.org>
2017-09-27 13:49:31 +0200

    fine-tune layout

--- pp/foot.php
+++ pp/foot.php
@@ -5,6 +5,8 @@ close_div(); // thePayload
 
 end_deliverable( 'htmlPayloadOnly' );
 
+close_div(); // theOutback
+
 open_div( 'id=theFooter' );
   open_table( 'css hfill' );
   open_tr();
@@ -12,8 +14,6 @@ open_div( 'id=theFooter' );
     open_td( 'right small', sprintf( "page %s: $now_mysql utc", ( $client_is_robot ? 'cached' : 'generated' ) ) );
   close_table();
 close_div();
-
-close_div(); // theOutback
 
 // insert an invisible submit button to allow to submit the update_form by pressing ENTER:
 open_span( 'nodisplay', html_tag( 'input', 'type=submit', NULL ) );
