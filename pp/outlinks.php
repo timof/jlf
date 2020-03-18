762f233cbb92627c3023856f98db184648861694
Timo Felbinger <gittf@qipc.org>
2020-03-18 07:42:34 +0100

    fix broken links

--- pp/outlinks.php
+++ pp/outlinks.php
@@ -27,21 +27,20 @@ define( 'URL_INSTGGRAPH_E', 'http://www.geographie.uni-potsdam.de' );
 
 
 
-define( 'URL_FAKJURA_D', 'http://www.uni-potsdam.de/fakultaeten/juristische-fakultaet.html' );
-define( 'URL_FAKJURA_E', 'http://www.uni-potsdam.de/fakultaeten/juristische-fakultaet.html' );
+define( 'URL_FAKJURA_D', 'http://www.uni-potsdam.de/de/jura' );
+define( 'URL_FAKJURA_E', 'http://www.uni-potsdam.de/en/jura' );
 
-define( 'URL_FAKPHIL_D', 'http://www.uni-potsdam.de/fakultaeten/philosophische-fakultaet.html' );
-define( 'URL_FAKPHIL_E', 'http://www.uni-potsdam.de/fakultaeten/philosophische-fakultaet.html' );
+define( 'URL_FAKPHIL_D', 'http://www.uni-potsdam.de/de/philfak' );
+define( 'URL_FAKPHIL_E', 'http://www.uni-potsdam.de/en/philfak' );
 
-define( 'URL_FAKHUM_D', 'http://www.uni-potsdam.de/fakultaeten/humanwissenschaftliche-fakultaet.html' );
-define( 'URL_FAKHUM_E', 'http://www.uni-potsdam.de/fakultaeten/humanwissenschaftliche-fakultaet.html' );
+define( 'URL_FAKHUM_D', 'http://www.uni-potsdam.de/de/humfak' );
+define( 'URL_FAKHUM_E', 'http://www.uni-potsdam.de/en/humfak' );
 
-define( 'URL_FAKWISO_D', 'http://www.uni-potsdam.de/fakultaeten/wirtschafts-und-sozialwissenschaftliche-fakultaet.html' );
-define( 'URL_FAKWISO_E', 'http://www.uni-potsdam.de/fakultaeten/wirtschafts-und-sozialwissenschaftliche-fakultaet.html' );
+define( 'URL_FAKWISO_D', 'http://www.uni-potsdam.de/de/wiso' );
+define( 'URL_FAKWISO_E', 'http://www.uni-potsdam.de/en/wiso' );
 
-define( 'URL_FAKMATNAT_D', 'http://www.uni-potsdam.de/fakultaeten/mathematisch-naturwissenschaftliche-fakultaet.html' );
-define( 'URL_FAKMATNAT_E', 'http://www.uni-potsdam.de/fakultaeten/mathematisch-naturwissenschaftliche-fakultaet.html' );
-
+define( 'URL_FAKMATNAT_D', 'http://www.uni-potsdam.de/de/mnfakul' );
+define( 'URL_FAKMATNAT_E', 'http://www.uni-potsdam.de/en/mnfakul' );
 
 
 ?>
